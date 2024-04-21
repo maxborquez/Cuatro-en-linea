618fc72a5b0c95f8c70d8d0987562282c2bfb83a
Maximiliano <max.borquezc@gmail.com>
2024-04-20 23:12:42 -0400

    demo profesor y demos server_client

---
+++ sockets_demo/server_client_C++/client.cpp
@@ -0,0 +1,53 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Usage: %s <ip> <port>\n", argv[0]);
+        return -1;
+    }
+
+    int PORT = atoi(argv[2]);
+    int status, valread, client_fd;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char message[1024];
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    printf("Enter a message: ");
+    fgets(message, 1024, stdin);
+    send(client_fd, message, strlen(message), 0);
+    printf("Message sent\n");
+
+    valread = read(client_fd, buffer, 1024 - 1); 
+    printf("Server response: %s\n", buffer);
+
+    close(client_fd);
+    return 0;
+}
---
+++ sockets_demo/server_client_C++/server.cpp
@@ -0,0 +1,61 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        return -1;
+    }
+
+    int PORT = atoi(argv[1]);
+    int server_fd, new_socket;
+    ssize_t valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024 - 1);
+    printf("Mensaje recibido: %s\n", buffer);
+
+    close(new_socket);
+
+    close(server_fd);
+    return 0;
+}
