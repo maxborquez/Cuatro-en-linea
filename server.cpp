c5ce591ba785ae285e538d62ad11a7e26787bcfc
Maximiliano <max.borquezc@gmail.com>
2024-04-24 02:20:08 -0400

    multiples conexiones

--- server.cpp
+++ server.cpp
@@ -1,7 +1,7 @@
 /*
  * server.cpp
  *
- * g++ -std=c++11 server.cpp -o server
+ * g++ -std=c++11 -pthread server.cpp -o server
  *
  */
 #include <sys/socket.h> // socket()
@@ -9,32 +9,33 @@
 #include <string.h>     // memset()
 #include <unistd.h>
 #include <iostream>
+#include <pthread.h>
 using namespace std;
-//
-void jugar(int socket_cliente, struct sockaddr_in direccionCliente) {
-    //
+
+void* jugar(void* args) {
+    int socket_cliente = *((int*)args);
+    struct sockaddr_in direccionCliente;
+    socklen_t addr_size = sizeof(struct sockaddr_in);
+    getpeername(socket_cliente, (struct sockaddr*)&direccionCliente, &addr_size);
+
     char buffer[1024];
     memset(buffer, '\0', sizeof(char)*1024);
     int n_bytes = 0;
 
-    //
     char ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(direccionCliente.sin_addr), ip, INET_ADDRSTRLEN);
-    //
+
     cout << "[" << ip << ":" << ntohs(direccionCliente.sin_port) << "] Nuevo jugador." << endl;
 
-    //
     while ((n_bytes = recv(socket_cliente, buffer, 1024, 0))) {
         buffer[n_bytes] = '\0';
 
-        //
         if (buffer[0] == 'Q') {
             cout << "[" << ip << ":" << ntohs(direccionCliente.sin_port) << "] Sale del juego." << endl;
             close(socket_cliente);
             break;
         }
 
-        //
         if (buffer[0] == 'c' && buffer[1] >= '1' && buffer[1] <= '7') {
             cout << "[" << ip << ":" << ntohs(direccionCliente.sin_port) << "] Columna: " << buffer[1] << endl;
             send(socket_cliente, "ok\n", 3, 0);
@@ -43,77 +44,59 @@ void jugar(int socket_cliente, struct sockaddr_in direccionCliente) {
             send(socket_cliente, "error\n", 6, 0);
         }
     }
+    close(socket_cliente);
+    pthread_exit(NULL);
 }
-//
+
 int main(int argc, char **argv) {
-    //
     if (argc < 2) {
         cerr << "Usage: " << argv[0] << " <port>" << endl;
         return 1;
     }
+
     int port = atoi(argv[1]);
     int socket_server = 0;
-    // socket address structures.
     struct sockaddr_in direccionServidor, direccionCliente;
 
-    // crea el socket.
-    /*
-     * domain:
-     *      AF_ LOCAL-> processes on the same host.
-     *      AF_INET -> processes on different hosts connected by IP (AF_INET->IPv4, AF_INET6->IPv6)
-     * type:
-     *      SOCK_STREAM: TCP (reliable, connection-oriented)
-     *      SOCK_DGRAM: UDP (unreliable, connectionless)
-     * protocol:
-     *      Protocol value for Internet Protocol(IP), which is 0.
-     */
     cout << "Creating listening socket ...\n";
     if ((socket_server = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         cout << "Error creating listening socket\n";
         exit(EXIT_FAILURE);
     }
 
-    // configuracion de los atributos de la clase sockaddr_in.
     cout << "Configuring socket address structure ...\n";
     memset(&direccionServidor, 0, sizeof(direccionServidor));
     direccionServidor.sin_family      = AF_INET;
     direccionServidor.sin_addr.s_addr = htonl(INADDR_ANY);
     direccionServidor.sin_port        = htons(port);
 
-    //
     cout << "Binding socket ...\n";
     if (bind(socket_server, (struct sockaddr *) &direccionServidor, sizeof(direccionServidor)) < 0) {
         cout << "Error calling bind()\n";
         exit(EXIT_FAILURE);
     }
 
-    //
     cout << "Calling listening ...\n";
     if (listen(socket_server, 1024) < 0) {
         cout << "Error calling listen()\n";
         exit(EXIT_FAILURE);
     }
 
-    // para obtener info del cliente.
-    socklen_t addr_size;
-    addr_size = sizeof(struct sockaddr_in);
-
-    //
     cout << "Waiting client request ...\n";
     while (true) {
-        /*  Wait for a connection, then accept() it  */
         int socket_cliente;
+        pthread_t thread_id;
+        socklen_t addr_size = sizeof(struct sockaddr_in);
 
-        //
         if ((socket_cliente = accept(socket_server, (struct sockaddr *)&direccionCliente, &addr_size)) < 0) {
             cout << "Error calling accept()\n";
             exit(EXIT_FAILURE);
         }
 
-        //
-        jugar(socket_cliente, direccionCliente);
+        if (pthread_create(&thread_id, NULL, jugar, (void*)&socket_cliente) < 0) {
+            cout << "Error creating thread\n";
+            exit(EXIT_FAILURE);
+        }
     }
-
-    //
     return 0;
 }
