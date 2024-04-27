b8f363b9ed7b86ff8b38b60326193f64db453417
Maximiliano <max.borquezc@gmail.com>
2024-04-27 07:05:43 -0400

    tablero, falta invertir

--- client.cpp
+++ client.cpp
@@ -5,10 +5,22 @@
 #include <unistd.h>
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    if (argc < 3)
-    {
+// Función para imprimir el tablero
+void printBoard(char board[6][7]) {
+    std::cout << "TABLERO" << std::endl;
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "-------------" << std::endl;
+    std::cout << "1 2 3 4 5 6 7" << std::endl;
+}
+
+
+int main(int argc, char const *argv[]) {
+    if (argc < 3) {
         printf("Usage: %s <ip> <port>\n", argv[0]);
         return -1;
     }
@@ -19,8 +31,15 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0};
     char message[1024];
 
-    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    // Inicializar el tablero vacío
+    char board[6][7];
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            board[i][j] = ' ';
+        }
+    }
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
@@ -28,24 +47,24 @@ int main(int argc, char const *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
-    {
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
-    {
+    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
 
     valread = read(client_fd, buffer, 1024 - 1);
     printf("%s", buffer);
-    memset(buffer, 0, sizeof(buffer)); 
+    memset(buffer, 0, sizeof(buffer));
 
     while(1) {
         if (message[0] != 'Q') {
+            // Imprimir el tablero antes de que el cliente ingrese su movimiento
+            printBoard(board);
             printf("Enter a message: ");
             fgets(message, 1024, stdin);
             send(client_fd, message, strlen(message), 0);
@@ -56,7 +75,26 @@ int main(int argc, char const *argv[])
                 break;
             }
 
+            // Actualizar el tablero local con el movimiento enviado por el cliente
+            int column_client = message[1] - '1';
+            for (int i = 0; i < 6; ++i) {
+                if (board[i][column_client] == ' ') {
+                    board[i][column_client] = 'C'; // 'C' indica movimiento del cliente
+                    break;
+                }
+            }
+
             valread = read(client_fd, buffer, 1024 - 1);
+            // Actualizar el tablero local con el movimiento recibido del servidor
+            int column_server = buffer[1] - '1';
+            for (int i = 0; i < 6; ++i) {
+                if (board[i][column_server] == ' ') {
+                    board[i][column_server] = 'S'; // 'S' indica movimiento del servidor
+                    break;
+                }
+            }
+            // Imprimir el tablero actualizado
+            printBoard(board);
             printf("Server response: %s\n", buffer);
             memset(buffer, 0, sizeof(buffer));
         } else {
